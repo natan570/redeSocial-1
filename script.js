aac41159b11a4c1504d283c0df6cae2de24c58ce
natan570 <natansollak@gmail.com>
2026-09-09 11:48:49 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     const likeBtn= document.querySelector(".like-btn");
+    const postMedia = document.querySelector(".post-media");
+    if (!likeBnt) return;
+
+    const likesCountSpan = likeBtn.querySelector(".likes-count");
+    const bookmarkBtn = document.querySelector(".bookmark-btn");
+
+    let isLiked = false;
+    let baseLikes = 0;
+    //texto inicial
+    if(likesCountSpan){
+        likesCountSpan.textContent = "0";
+    }
+
+    //numeros acima de 1mil
+    function formatLikes (num){
+        if(num >=1000){
+            return(num/1000).toFixed(1)+"K"; 
+        }      
+        return num.toString();
+
+
+
+
+
+
+
 })
